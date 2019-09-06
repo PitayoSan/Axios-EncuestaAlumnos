ebfdff5395ad69b6299c3cb7ce8024dde65f35aa
Karol Gutierrez <kjgs.22@gmail.com>
2019-09-06 06:46:55 -0500

    Save answers as .csv file"

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-// function saveName(){
-//     var document.getElementById('nombre');
-// }
-
 var data = {
-    'escuela': '', //'Jesus Reyes Heroles'
-    'grado': '', //6
-    'grupo': '', //B
-    'turno': '', //V
+    'escuela': '',
+    'grado': '',
+    'grupo': '',
+    'turno': '',
     'aPaterno' : '',
     'aMaterno' : '',
     'nombres' : '',
@@ -22,32 +18,58 @@ var data = {
     'telefono' : '',
     'celular' : ''
 };
-  
-  
-function saveAs(text, filename){
-    var pom = document.createElement('a');
-    pom.setAttribute('href', 'data:document/json;charset=urf-8,'+encodeURIComponent(text));
-    pom.setAttribute('download', filename);
-    pom.click();
-};
+   
+function download(data, filename, type) {
+    var file = new Blob([data], {type: type});
+    if (window.navigator.msSaveOrOpenBlob) // IE10+
+        window.navigator.msSaveOrOpenBlob(file, filename);
+    else { // Others
+        var a = document.createElement("a"),
+                url = URL.createObjectURL(file);
+        a.href = url;
+        a.download = filename;
+        document.body.appendChild(a);
+        a.click();
+        setTimeout(function() {
+            document.body.removeChild(a);
+            window.URL.revokeObjectURL(url);  
+        }, 0); 
+    }
+}
 
 function sendAnswers(){
     var x = document.getElementById("frm1");
     for (i = 0; i < x.length ;i++) {
         data[x.elements[i].name] += x.elements[i].value;
     }
-
     var y = document.getElementById("frm2");
     for (i = 0; i < y.length ;i++) {
         data[y.elements[i].name] += y.elements[i].value;
     }
-
     var z = document.getElementById("frm3");
     for (i = 0; i < z.length ;i++) {
         data[z.elements[i].name] += z.elements[i].value;
     }
-
-    saveAs(JSON.stringify(data), 'data.json');
-    console.log(data)
+    const keys = Object.keys(data);
+    console.log(keys.length);
+    var answers = new Array(keys.length);
+    for (var i = 0; i < answers.length; i++) {
+        answers[i] = new Array(2);
+    }
+    var csv="";
+    for (var i = 0; i < answers.length; i++) {
+        answers[i][0] = keys[i];
+        csv+=keys[i];
+        csv+=',';
+        answers[i][1] = data[keys[i]];
+    }
+    csv+="\r\n";
+    for (var i = 0; i < answers.length; i++) {
+        csv+=data[keys[i]];
+        csv+=',';
+    }
+    csv+="\r\n";
+    download(csv, 'respuestas.csv', 'data:text/csv;charset=urf-8');
+    console.log(csv)
 }
       
